8d47149ebe349bf87e3dcef4c27986644d33a439
Wesley Stephan <1919091stephan@zuyd.nl>
2020-11-01 14:07:34 +0100

    Add login page.

---
+++ Casusblok1b2egroep3/Inloggen.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Casusblok1b2egroep3
+{
+    public partial class Profiel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("UserID");
+            cookie.Value = GridView1.Rows[0].Cells[0].Text;
+            Response.Cookies.Add(cookie);
+            Response.Redirect("Home.aspx");
+        }
+
+    }
+}
