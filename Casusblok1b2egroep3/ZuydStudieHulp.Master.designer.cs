92ddabea23c4f355379485b8751356807f4358b7
RoyWendries <1747290wendries@zuyd.nl>
2020-11-02 10:47:03 +0100

    changes

--- Casusblok1b2egroep3/ZuydStudieHulp.Master.designer.cs
+++ Casusblok1b2egroep3/ZuydStudieHulp.Master.designer.cs
@@ -11,8 +11,17 @@ namespace Casusblok1b2egroep3
 {
 
 
-    public partial class Profiel
+    public partial class Site1
     {
+
+        /// <summary>
+        /// head control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
         /// form1 control.
@@ -22,5 +31,14 @@ namespace Casusblok1b2egroep3
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
