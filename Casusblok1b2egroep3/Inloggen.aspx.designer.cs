626be95c26acaaf7f517c5eebb73ce837246e127
Wesley Stephan <1919091stephan@zuyd.nl>
2020-11-03 12:44:45 +0100

    Login page updated.

--- Casusblok1b2egroep3/Inloggen.aspx.designer.cs
+++ Casusblok1b2egroep3/Inloggen.aspx.designer.cs
@@ -15,24 +15,6 @@ namespace Casusblok1b2egroep3
     {
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// SqlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
-
-        /// <summary>
         /// LabelEmail control.
         /// </summary>
         /// <remarks>
@@ -69,15 +51,6 @@ namespace Casusblok1b2egroep3
         protected global::System.Web.UI.WebControls.TextBox txtbPassword;
 
         /// <summary>
-        /// Login1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Login Login1;
-
-        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
@@ -94,5 +67,14 @@ namespace Casusblok1b2egroep3
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnLogin;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
